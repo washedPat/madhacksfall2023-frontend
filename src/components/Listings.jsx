a216ef5c0e30919613a43b3c1d88e9515c9fd416
mrprabbs <pkukreja2002@gmail.com>
2023-11-11 17:23:48 -0600

    styling for listing and started myrentals and register-listing

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -5,6 +5,10 @@ import './listings.css'; // Import the regular CSS file
 
 const Listings = () => {
   const [listings, setListings] = useState([]);
+  const [distance, setDistance] = useState(0);
+  const [price, setPrice] = useState(0); // State for pricing
+  const [vehicleType, setVehicleType] = useState('');
+  const [rating, setRating] = useState(0);
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -64,21 +68,83 @@ const Listings = () => {
     setListings(simulatedListings); // Update the component state with simulated data
   }, []);
 
+  const handleDistanceChange = (e) => {
+    setDistance(e.target.value);
+  };
+
+  const handlePriceChange = (e) => {
+    setPrice(e.target.value);
+  };
+  const handleVehicleTypeChange = (e) => {
+    setVehicleType(e.target.value);
+  };
+  const handleRatingChange = (e) => {
+    setRating(e.target.value);
+  };
+
   return (
     <div className="mainContainer">
       <div className="bodyContainer">
         <div className="filterContainer">
           {/* Side box for filtering */}
-          <div className="filterBox">
-            <label htmlFor="location">Current Location:</label>
-            <input type="text" id="location" name="location" />
-            {/* Add more filter options as needed */}
-            <p>Location Slider Bar</p>
-          </div>
+          <label htmlFor="location">Current Location:</label>
+          <input type="text" id="location" name="location" />
+
+          {/* Distance slider */}
+          <label htmlFor="distance">Distance: {distance} miles</label>
+          <input
+            type="range"
+            id="distance"
+            name="distance"
+            min="0"
+            max="100"
+            value={distance}
+            onChange={handleDistanceChange}
+          />
+
+          {/* Price slider */}
+          <label htmlFor="price">Price: ${price}</label>
+          <input
+            type="range"
+            id="price"
+            name="price"
+            min="0"
+            max="100"
+            value={price}
+            onChange={handlePriceChange}
+          />
+
+          {/* Dropdown selector for vehicle type */}
+          <label htmlFor="vehicleType">Vehicle Type:</label>
+          <select
+            id="vehicleType"
+            name="vehicleType"
+            value={vehicleType}
+            onChange={handleVehicleTypeChange}
+          >
+            <option value="">Select Vehicle Type</option>
+            <option value="SUV">SUV</option>
+            <option value="Sedan">Sedan</option>
+            <option value="EV">EV (Electric Vehicle)</option>
+            <option value="Minivan">Minivan</option>
+          </select>
+
+          {/* Rating slider */}
+          <label htmlFor="rating">Rating: {rating}</label>
+          <input
+            type="range"
+            id="rating"
+            name="rating"
+            min="0"
+            max="5"
+            step="0.1"
+            value={rating}
+            onChange={handleRatingChange}
+          />
         </div>
         <div className="listingContainer">
           {/* Grid of listing cards */}
-          {listings.map(listing => (
+          {listings.map((listing) => (
             <div key={listing._id} className="listingCard">
               <img src={listing.imageUrl} alt="Parking Spot" className="listingImage" />
               <h3 className="listingTitle">{listing.location}</h3>
--- src/components/MyRentals.jsx
+++ src/components/MyRentals.jsx
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'; // Import the Link component from react-router-dom
+import './listings.css';
 
-function MyRentals() {
+const MyRentals = () => {
+  const [myListings, setMyListings] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const simulatedMyListings = [
+      {
+        _id: '1',
+        imageUrl: 'URL_TO_IMAGE_1',
+        location: 'My Downtown Spot',
+        type: 'Covered',
+        price: '$25/day',
+        rating: 4.7,
+      },
+      {
+        _id: '2',
+        imageUrl: 'URL_TO_IMAGE_2',
+        location: 'Home Garage',
+        type: 'Garage',
+        price: '$20/day',
+        rating: 4.2,
+      },
+      // Add more simulated listings as needed
+    ];
+
+    setMyListings(simulatedMyListings);
+  }, []);
+
   return (
-
-    <div>
-          <h1 className="text-3xl font-bold">MY RENTALS</h1>
+    <div className="mainContainer">
+      <div className="bodyContainer">
+        <div className="addBookingButton">
+          {/* Use the Link component from react-router-dom for navigation */}
+          <Link to="/register-listing">
+            <a className="addButton">+</a>
+          </Link>
+        </div>
+        <div className="myListingsContainer">
+          {/* Grid of My Listings cards */}
+          {myListings.map((listing) => (
+            <div key={listing._id} className="listingCard">
+              <img src={listing.imageUrl} alt="Parking Spot" className="listingImage" />
+              <h3 className="listingTitle">{listing.location}</h3>
+              <p className="listingInfo">Type: {listing.type}</p>
+              <p className="listingInfo">Price: {listing.price}</p>
+              <p className="listingInfo">Rating: {listing.rating}</p>
+              <button className="editButton">Edit</button>
+            </div>
+          ))}
+        </div>
+      </div>
+      {error && <p>Error Code: {error}</p>}
     </div>
-   
   );
 };
 
