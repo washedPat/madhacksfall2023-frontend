4ee581286b5f8748978875c3ed8cd95fe67691a0
ntalluri <nehatalluri@live.com>
2023-11-11 22:08:05 -0600

    Booking Now page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Card({ listing }) {
+  const navigate = useNavigate();
+  const handleBooking = () => {
+    navigate('/BookingNow', { state: { listing } });
+  };
   return (
     <div className="listingCard">
       <img src={listing.imageUrl} alt="Parking Spot" className="listingImage" />
-      <h3 className="listingTitle">{listing.location}</h3>
-      <p className="listingInfo">Type: {listing.type}</p>
+      <h3 className="listingTitle">{listing.title}</h3>
+      <p className="listingInfo">Parking Size: {listing.parking_size}</p>
       <p className="listingInfo">Price: {listing.price}</p>
-      <p className="listingInfo">Rating: {listing.rating}</p>
-      <button className="listingButton">Book Now</button>
+      <p className="listingInfo">Description: {listing.description}</p>
+      <p className='listingInfo'>Date: {listing.start_date} - {listing.end_date}</p>
+      <p className='listingInfo'>Address: {listing.street}, {listing.city}, {listing.state} {listing.zip} {listing.country}</p>
+      <button className="listingButton" onClick={handleBooking}>Book Now</button>
     </div>
   );
 }
