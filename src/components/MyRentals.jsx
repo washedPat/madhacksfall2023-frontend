7f290ce65557fe47e269e5fdab87ee1220cd3cab
ntalluri <nehatalluri@live.com>
2023-11-11 20:07:51 -0600

    made changes

--- src/components/MyRentals.jsx
+++ src/components/MyRentals.jsx
@@ -1,61 +1,65 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext} from 'react';
 import { Link } from 'react-router-dom'; // Import the Link component from react-router-dom
-import './listings.css';
+import './myrentals.css';
+import RentalsCard from './RentalsCard';
+import LoggedIn from '../contexts/loggedin';
+
 
 const MyRentals = () => {
   const [myListings, setMyListings] = useState([]);
   const [error, setError] = useState(null);
+  const [loggedIn, setLoggedIn] = useContext(LoggedIn);
+  console.log(loggedIn)
 
+  console.log(loggedIn)
   useEffect(() => {
-    const simulatedMyListings = [
-      {
-        _id: '1',
-        imageUrl: 'URL_TO_IMAGE_1',
-        location: 'My Downtown Spot',
-        type: 'Covered',
-        price: '$25/day',
-        rating: 4.7,
-      },
-      {
-        _id: '2',
-        imageUrl: 'URL_TO_IMAGE_2',
-        location: 'Home Garage',
-        type: 'Garage',
-        price: '$20/day',
-        rating: 4.2,
-      },
-      // Add more simulated listings as needed
-    ];
+    if (loggedIn) { // Only simulate listings if the user is logged in
+      const simulatedMyListings = [
+        {
+          _id: '1',
+          imageUrl: 'URL_TO_IMAGE_1',
+          location: 'My Downtown Spot',
+          type: 'Covered',
+          price: '$25/day',
+          rating: 4.7,
+        },
+        {
+          _id: '2',
+          imageUrl: 'URL_TO_IMAGE_2',
+          location: 'Home Garage',
+          type: 'Garage',
+          price: '$20/day',
+          rating: 4.2,
+        },
+      ];
 
-    setMyListings(simulatedMyListings);
-  }, []);
+      setMyListings(simulatedMyListings);
+    }
+  }, [loggedIn]);
 
-  return (
-    <div className="mainContainer">
-      <div className="bodyContainer">
-        <div className="addBookingButton">
-          {/* Use the Link component from react-router-dom for navigation */}
-          <Link to="/RegisterListing">
-            <a className="addButton">+</a>
-          </Link>
+  if (loggedIn === null) {
+    return <div className="mainContainer">PLEASE LOGIN</div>;
+  
+  } else {
+
+    return (
+      <div className="mainContainer">
+        <div className="bodyContainer">
+          <div className="addBookingButton">
+          <Link to="/RegisterListing" className="addButton">
+              +
+            </Link>
+          </div>
+          <div className="myListingsContainer">
+            {myListings.map((listing) => (
+              <RentalsCard key={listing._id} listing={listing} /> 
+            ))}
+          </div>
         </div>
-        <div className="myListingsContainer">
-          {/* Grid of My Listings cards */}
-          {myListings.map((listing) => (
-            <div key={listing._id} className="listingCard">
-              <img src={listing.imageUrl} alt="Parking Spot" className="listingImage" />
-              <h3 className="listingTitle">{listing.location}</h3>
-              <p className="listingInfo">Type: {listing.type}</p>
-              <p className="listingInfo">Price: {listing.price}</p>
-              <p className="listingInfo">Rating: {listing.rating}</p>
-              <button className="editButton">Edit</button>
-            </div>
-          ))}
-        </div>
+        {error && <p>Error Code: {error}</p>}
       </div>
-      {error && <p>Error Code: {error}</p>}
-    </div>
-  );
+    );
+  }
 };
 
 export default MyRentals;
