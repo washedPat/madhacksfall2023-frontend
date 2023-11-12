d602206f744cef9b0b8274db44005416852b9387
ntalluri <nehatalluri@live.com>
2023-11-12 11:26:14 -0600

    fixed more

--- src/components/register-listing.jsx
+++ src/components/register-listing.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Listings.css';
-import './registerlisting.css'
+import './registerListing.css'
 
 const RegisterListing = () => {
   const navigate = useNavigate();
